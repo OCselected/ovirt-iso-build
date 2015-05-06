be5207f5e4719c426094f0beda62a60bf506b981
blkart <blkart.org@gmail.com>
2015-05-05 21:44:50 -0400

    备份ISO目录
    Signed-off-by: blkart <blkart.org@gmail.com>

--- buildinstall.sh
+++ buildinstall.sh
@@ -2,4 +2,8 @@
 
 . ./environ.sh
 
+if [ -e $OVIRT_ALLINONE_BASE/$ISO_DATA_PATH ]; then
+    mv $OVIRT_ALLINONE_BASE/$ISO_DATA_PATH $OVIRT_ALLINONE_BASE/$ISO_DATA_PATH-$DATESTRING
+fi
+
 lorax --version $VERSION --product $PRODUCT_NAME --release $RELEASE --isfinal -s file:///ovirt/iso_dir/ $OVIRT_ALLINONE_BASE/$ISO_DATA_PATH
